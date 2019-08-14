ee5a63ba650f57d2d8172d46cceb3b24e30d3dd2
David Strauch <dmaster18@gmail.com>
2019-08-14 17:45:06 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -31,8 +31,8 @@ class String
   end
 end
 
-puts "Hello, my name is John. How are you doing today? I am great! However, I am not so certain about that anymore".count_sentences.class
+puts "Hello, my name is John. How are you doing today? I am great! However, I am not so certain about that anymore".count_sentences
 
-puts "returns zero if there are no sentences in a string".count_sentences.class
+puts "returns zero if there are no sentences in a string".count_sentences
 
 "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
