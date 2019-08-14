0ff61a879f9178104bfd37856656716e8beb172b
David Strauch <dmaster18@gmail.com>
2019-08-14 17:40:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -31,4 +31,4 @@ class String
   end
 end
 
-puts "Hello, my name is John. How are you doing today? I am great! However, I am not so certain about that anymore".
+puts "Hello, my name is John. How are you doing today? I am great! However, I am not so certain about that anymore".count_sentences
